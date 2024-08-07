77a9158d54226fbf13385144923ce64167240046
geeksgood <jayshah21061991@gmail.com>
2024-08-07 17:57:03 +0530

    first draft

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+};
